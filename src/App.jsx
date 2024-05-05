7568d8bd123e235ff405ef6147866cbe2ab96e6e
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-05-05 13:35:28 -0700

    Eat-n-split

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+const initialFriends = [
+  {
+    id: 200,
+    name: "Yakubu",
+    image:
+      "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=800",
+    balance: -7,
+  },
+  {
+    id: 204,
+    name: "Julie",
+    image:
+      "https://images.pexels.com/photos/1590483/pexels-photo-1590483.jpeg?auto=compress&cs=tinysrgb&w=800",
+    balance: 20,
+  },
+  {
+    id: 207,
+    name: "Peter",
+    image:
+      "https://images.pexels.com/photos/1906157/pexels-photo-1906157.jpeg?auto=compress&cs=tinysrgb&w=800",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
