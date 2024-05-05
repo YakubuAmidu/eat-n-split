65ece326b74cefddd70e65d5c9c00d5c4b62646d
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-05-05 14:48:17 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -87,6 +88,30 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with a x</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍 Your expense</label>
+      <input type="text" />
+
+      <label>👯‍♂️ X'S expense</label>
+      <input type="text" disabled />
+
+      <label>😁 Who is paying the bill ?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split bill</Button>
     </form>
   );
 }
